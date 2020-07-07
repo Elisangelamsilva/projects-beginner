f321e00ebeb71ba9ff8ebc37f408273b0319fb9d
Elisangela <elisangela00silva@gmail.com>
2020-07-07 14:25:55 -0300

    capturação do evento click e não ter nada

---
+++ Bin2Dec/script.js
@@ -0,0 +1,6 @@
+const binario = document.querySelector("#input-binario")
+const btnConverter = document.querySelector("#btn-converter")
+
+btnConverter.addEventListener("click", function(){
+    if(!binario.value) return;
+})
