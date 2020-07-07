e6801a8db51661259bfe6249a876141ca0b54827
Elisangela <elisangela00silva@gmail.com>
2020-07-07 17:24:30 -0300

    lógica em funcionamento

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -1,6 +1,16 @@
-const binario = document.querySelector("#input-binario")
-const btnConverter = document.querySelector("#btn-converter")
+function btnConverter(){
+let binario = Number(document.querySelector(".inputBinario").value)
+let resposta = document.querySelector(".resposta")
 
-btnConverter.addEventListener("click", function(){
-    if(!binario.value) return;
-})
+console.log(binario.toString().length)
+
+if(binario.toString().length > 8) alert("Insira até 8 dígitos."); 
+//if(binario !== 1 && binario !== 0) alert("É aceito apenas 0 e 1.")
+
+let decimal = parseInt(binario, 2);
+
+ 
+resposta.innerHTML = `O binário ${binario} convertido para decimal é ${decimal}.`
+
+
+}
