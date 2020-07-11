0c9fde619cc15764be535c45b5c58c0284f3ef59
Elisangela <elisangela00silva@gmail.com>
2020-07-11 16:12:59 -0300

    Update script.js

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -1,10 +1,18 @@
 function btnConverter(){
-let binario = Number(document.querySelector(".inputBinario").value)
+let binario = document.querySelector(".inputBinario").value
 let resposta = document.querySelector(".resposta")
 
-
-if(binario.toString().length > 8) alert("Insira até 8 dígitos."); 
+if(binario > 8) alert("Insira até 8 dígitos."); 
 //if(binario !== 1 && binario !== 0) alert("É aceito apenas 0 e 1.")
+
+for(let i = 0; i < 7; i++){
+    if(i === 0){
+        binario.charAt(i)
+    }
+    if(binario === 1){
+
+    }
+}
 
 let decimal = parseInt(binario, 2);
 
