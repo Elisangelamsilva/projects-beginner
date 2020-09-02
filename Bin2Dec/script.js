8b0d36d99a5d5ba76dfb307f0e7482bc15fe2a67
Elisangela <elisangela00silva@gmail.com>
2020-09-02 15:45:16 -0300

    edição Bin2Dec

--- Bin2Dec/script.js
+++ Bin2Dec/script.js
@@ -5,14 +5,13 @@ let resposta = document.querySelector(".resposta")
 if(binario > 8) alert("Insira até 8 dígitos."); 
 //if(binario !== 1 && binario !== 0) alert("É aceito apenas 0 e 1.")
 
-for(let i = 0; i < 7; i++){
-    if(i === 0){
-        binario.charAt(i)
-    }
-    if(binario === 1){
-
-    }
-}
+// for(let i = 0; i < 7; i++){
+//     if(i === 0){
+//         binario.charAt(i)
+//     }
+//     if(binario === 1){
+//     }
+// }
 
 let decimal = parseInt(binario, 2);
 
